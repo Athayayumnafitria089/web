f5db10d93da2be870f7f3aa5d8eb21cbede42a6e
Athaya Yumna Fitria <tayataya0312@gmail.com>
2025-03-17 07:58:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll("nav ul li a").forEach(link => {
-    link.addEventListener("mouseover", () => {
-        link.style.color = "red";
-    });
-    link.addEventListener("mouseout", () => {
-        link.style.color = "black";
-    });
- });
