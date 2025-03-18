8464bad44f395bed794f8102512728970148605f
Athaya089 <tayataya0312@gmail.com>
2025-03-18 16:59:22 +0700

    menambahkan library.html dan script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.querySelectorAll("nav ul li a").forEach(link => {
     link.addEventListener("mouseover", () => {
+        speak(link.textContent); // Membaca teks tautan
         link.style.color = "red";
     });
     link.addEventListener("mouseout", () => {
         link.style.color = "black";
     });
  });
+ 
+ // Fungsi untuk membaca teks
+ function speak(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+ }
